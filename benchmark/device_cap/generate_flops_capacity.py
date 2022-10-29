1b6aec77cfd575afd6e9ca84dcd3bb0f97392fef
yuxuanzh <yuxuanzh@h1.evofed-openimg.gaia-pg0.wisc.cloudlab.us>
2022-10-29 07:07:36 -0500

    add device capacity file

--- benchmark/device_cap/generate_flops_capacity.py
+++ benchmark/device_cap/generate_flops_capacity.py
@@ -13,9 +13,9 @@ outputClass = {'Mnist': 10, 'cifar10': 10, "imagenet": 1000, 'emnist': 47, 'amaz
                }
 
 dataset_info = {
-    'femnist': (1, 3, 28, 28),
-    'openimage': (1, 3, 256, 256),
-    'speech': (32, 32)
+    'femnist': (1, 3, 28, 28)
+    # 'openimage': (1, 3, 256, 256),
+    # 'speech': (32, 32)
 }
 
 model_zoo = {
@@ -33,13 +33,25 @@ for dataset in dataset_info:
         os.mkdir(dataset)
     dummy_input = torch.randn(dataset_info[dataset])
     min_macs, min_params = profile(model_zoo['nasbench'], inputs=(dummy_input, ), verbose=False)
+    print(f"min MACs: {min_macs}, min params: {min_params}")
     for model_name in ['resnet18', 'shufflenet']:
         max_macs, max_params = profile(model_zoo['resnet18'], inputs=(dummy_input, ), verbose=False)
+        print(f"max MACs: {max_macs}, max MACs: {max_params}")
+        # normal distribution
         client_cap_macs = np.random.normal(loc=(min_macs+max_macs)/2, scale=(min_macs+max_macs)/2/2, size=n_client)
         client_cap_params = np.random.normal(loc=(min_params+max_params)/2, scale=(min_params+max_params)/2/2, size=n_client)
         client_ids = list(range(0,n_client))
         records = list(zip(client_ids, client_cap_macs, client_cap_params))
-        with open(f'{dataset}/{model_name}.csv', 'w') as f:
+        with open(f'{dataset}/{model_name}-normal.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['id', 'macs', 'params'])
+            writer.writerows(records)
+        # uniform distribution
+        client_cap_macs = np.random.uniform(low=min_macs, high=max_macs, size=n_client)
+        client_cap_params = np.random.uniform(low=min_macs, high=max_macs, size=n_client)
+        client_ids = list(range(0,n_client))
+        records = list(zip(client_ids, client_cap_macs, client_cap_params))
+        with open(f'{dataset}/{model_name}-uniform.csv', 'w') as f:
             writer = csv.writer(f)
             writer.writerow(['id', 'macs', 'params'])
             writer.writerows(records)
