0019a3065e885333b35e47f8862f247fdb5859da
yuxuanzh <yuxuanzh@h2.baselines.michigan-bigdata-pg0.wisc.cloudlab.us>
2023-03-02 13:21:35 -0600

    add checkout

--- docker/prepare_model.py
+++ docker/prepare_model.py
@@ -2,6 +2,9 @@ import torch.nn as nn
 import pickle
 import sys
 
+from thop import profile
+import torch
+
 
 def weight_reset(m):
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
@@ -12,6 +15,8 @@ model_path = sys.argv[1]
 
 with open(model_path, 'rb') as f:
     model = pickle.load(f)
-model.apply(weight_reset)
-with open(model_path, 'wb') as f:
-    pickle.dump(model, f)
+macs, profile = profile(model, inputs=(torch.randn(10, 3, 28, 28)))
+print(macs)
+# model.apply(weight_reset)
+# with open(model_path, 'wb') as f:
+#     pickle.dump(model, f)
