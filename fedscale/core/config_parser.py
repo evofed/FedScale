3da35bbda886ea160a6d162af4a980bd748ef86e
yuxuanzh <yuxuanzh@h1.ablation.michigan-bigdata-pg0.wisc.cloudlab.us>
2023-04-28 14:28:01 -0500

    add nocoefficient soft agg

--- fedscale/core/config_parser.py
+++ fedscale/core/config_parser.py
@@ -238,6 +238,7 @@ parser.add_argument('--starting_width_scale', type=int, default=1)
 parser.add_argument('--local_training', type=str, default="step")
 parser.add_argument('--layer_policy', type=str, default="gradient")
 parser.add_argument('--weight_mode', type=str, default="inherit")
+parser.add_argument('--agg_mode', type=str, default="decay")
 
 args, unknown = parser.parse_known_args()
 args.use_cuda = eval(args.use_cuda)
