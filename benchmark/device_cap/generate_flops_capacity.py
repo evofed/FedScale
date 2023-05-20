7b551baf0e1a820a72bcaca98a039d940db9db3e
yuxuan <yuxuanzh@umich.edu>
2023-05-20 12:48:04 +0800

    refine device cap

--- benchmark/device_cap/generate_flops_capacity.py
+++ benchmark/device_cap/generate_flops_capacity.py
@@ -15,48 +15,59 @@ outputClass = {'Mnist': 10, 'cifar10': 10, "imagenet": 1000, 'emnist': 47, 'amaz
                }
 
 dataset_info = {
-    # 'femnist': (1, 3, 28, 28),
-    # 'openimage': (1, 3, 256, 256),
+    'femnist': (1, 3, 28, 28),
+    'cifar10': (1, 3, 32, 32),
     'speech': (1, 1, 32, 32)
 }
 
 model_zoo = {
-    # 'resnet18': models.resnet18(),
-    'resnet34_speech': resnet34(num_classes=outputClass['google_speech'], in_channels=1),
-    'resnet34': models.resnet34(),
-    'shufflenet': models.shufflenet_v2_x2_0(),
-    # 'mobilenet': models.mobilenet_v2(),
-    # 'nasbench': nasbench.get_cell_based_tiny_net(config)
-    'small_resnet': small_resnet18(num_classes=outputClass['openImg']),
-    "small_resnet18_speech": small_resnet18_speech(num_classes=outputClass['openImg'])
+    'resnet_18': models.resnet18(),
+    'resnet_34_speech': resnet34(num_classes=outputClass['google_speech'], in_channels=1),
+    'resnet_s_speech': small_resnet18_speech(),
+    'mobilenetv3_s': models.mobilenet_v3_small(),
+    'nasbench': nasbench.get_cell_based_tiny_net(config)
+}
+
+small_model = {
+    'femnist': 'nasbench',
+    'cifar': 'mobilenetv3_s',
+    'speech': 'resnet_s_speech'
+}
+
+large_model = {
+    'femnist': 'resnet_18',
+    'cifar': 'resnet_18',
+    'speech': 'resnet_34_speech'
 }
 
 n_client = 500000
 
 for dataset in dataset_info:
-    if not os.path.exists(dataset):
-        os.mkdir(dataset)
+    if not os.path.exists(dataset+"_cap"):
+        os.mkdir(dataset+"_cap")
+    small_model_str = small_model[dataset]
+    large_model_str = large_model[dataset]
     dummy_input = torch.randn(dataset_info[dataset])
-    min_macs, min_params = profile(model_zoo['small_resnet18_speech'], inputs=(dummy_input, ), verbose=False)
-    print(f"min MACs: {min_macs}, min params: {min_params}")
-    for model_name in ['resnet34_speech']:
-        max_macs, max_params = profile(model_zoo[model_name], inputs=(dummy_input, ), verbose=False)
-        print(f"max MACs: {max_macs}, max params: {max_params}")
-        # normal distribution
-        client_cap_macs = np.random.normal(loc=(min_macs+max_macs)/2, scale=(min_macs+max_macs)/2/2, size=n_client)
-        client_cap_params = np.random.normal(loc=(min_params+max_params)/2, scale=(min_params+max_params)/2/2, size=n_client)
-        client_ids = list(range(0,n_client))
-        records = list(zip(client_ids, client_cap_macs, client_cap_params))
-        with open(f'{dataset}/{model_name}-normal.csv', 'w') as f:
-            writer = csv.writer(f)
-            writer.writerow(['id', 'macs', 'params'])
-            writer.writerows(records)
-        # uniform distribution
-        client_cap_macs = np.random.uniform(low=min_macs, high=max_macs, size=n_client)
-        client_cap_params = np.random.uniform(low=min_macs, high=max_macs, size=n_client)
-        client_ids = list(range(0,n_client))
-        records = list(zip(client_ids, client_cap_macs, client_cap_params))
-        with open(f'{dataset}/{model_name}-uniform.csv', 'w') as f:
-            writer = csv.writer(f)
-            writer.writerow(['id', 'macs', 'params'])
-            writer.writerows(records)
+    min_macs, min_params = profile(model_zoo[small_model_str], inputs=(dummy_input, ), verbose=False)
+    print(min_macs, min_params)
+    max_macs, max_params = profile(model_zoo[large_model_str], inputs=(dummy_input, ), verbose=False)
+    print(max_macs, max_params)
+    # normal distribution
+    client_cap_macs = np.random.normal(loc=(min_macs+max_macs)/2, scale=(min_macs+max_macs)/2/2, size=n_client)
+    client_cap_params = np.random.normal(loc=(min_params+max_params)/2, scale=(min_params+max_params)/2/2, size=n_client)
+    client_ids = list(range(0,n_client))
+    records = list(zip(client_ids, client_cap_macs, client_cap_params))
+    with open(f'{dataset}_cap/normal.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['id', 'macs', 'params'])
+        writer.writerows(records)
+    
+    # uniform distribution
+    client_cap_macs = np.random.uniform(low=min_macs, high=max_macs, size=n_client)
+    client_cap_params = np.random.uniform(low=min_macs, high=max_macs, size=n_client)
+    client_ids = list(range(0,n_client))
+    records = list(zip(client_ids, client_cap_macs, client_cap_params))
+    with open(f'{dataset}_cap/uniform.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['id', 'macs', 'params'])
+        writer.writerows(records)
--- benchmark/device_cap/generate_flops_capacity_realistic.py
+++ benchmark/device_cap/generate_flops_capacity_realistic.py
@@ -1,6 +1,7 @@
 import pickle
 import torchvision.models as models
 from fedscale.utils.models.specialized.resnet_speech import resnet34
+from fedscale.utils.models.evofed.small_resnet18_speech import small_resnet18_speech
 import os
 from thop import profile
 import numpy as np
@@ -16,53 +17,92 @@ outputClass = {'Mnist': 10, 'cifar10': 10, "imagenet": 1000, 'emnist': 47, 'amaz
                }
 
 dataset_info = {
-    'femnist': (1, 3, 28, 28)
-    # 'openimage': (1, 3, 256, 256),
-    # 'speech': (32, 32)
+    'femnist': (1, 3, 28, 28),
+    'cifar10': (1, 3, 32, 32),
+    'speech': (1, 1, 32, 32)
 }
 
 model_zoo = {
-    'resnet18': models.resnet18(),
-    # 'resnet34': resnet34(num_classes=outputClass['google_speech'], in_channels=1),
-    'shufflenet': models.shufflenet_v2_x2_0(),
-    # 'mobilenet': models.mobilenet_v2(),
+    'resnet_18': models.resnet18(),
+    'resnet_34_speech': resnet34(num_classes=outputClass['google_speech'], in_channels=1),
+    'resnet_s_speech': small_resnet18_speech(),
+    'mobilenetv3_s': models.mobilenet_v3_small(),
     'nasbench': nasbench.get_cell_based_tiny_net(config)
 }
 
+small_model = {
+    'femnist': 'nasbench',
+    'cifar10': 'mobilenetv3_s',
+    'speech': 'resnet_s_speech'
+}
+
+large_model = {
+    'femnist': 'resnet_18',
+    'cifar10': 'resnet_18',
+    'speech': 'resnet_34_speech'
+}
+
 computation = {}
+communication  = {}
 with open('../dataset/data/device_info/client_device_capacity', 'rb') as f:
     device_cap = pickle.load(f)
 
-caps = []
+# normalize computation
+comp_caps = []
 for client_id in device_cap:
-    cap = float(device_cap[client_id]['computation'])
-    caps.append(cap)
-    computation[client_id] = cap
-min_cap = min(caps)
-max_cap = max(caps)
+    comp_cap = float(device_cap[client_id]['computation'])
+    comp_caps.append(comp_cap)
+    computation[client_id] = comp_cap
+min_cap = min(comp_caps)
+max_cap = max(comp_caps)
 for client_id in computation:
     computation[client_id] = (computation[client_id] - min_cap) / (max_cap - min_cap)
 
+# normalize communication
+comm_caps = []
+for client_id in device_cap:
+    comm_cap = float(device_cap[client_id]['communication'])
+    comm_caps.append(comm_cap)
+    communication[client_id] = comm_cap
+min_cap = min(comm_caps)
+max_cap = max(comm_caps)
+for client_id in communication:
+    communication[client_id] = (communication[client_id] - min_cap) / (max_cap - min_cap)
+
+capacities = {}
+caps = []
+for client_id in computation:
+    capacities[client_id] = communication[client_id] + computation[client_id]
+    caps.append(capacities[client_id])
+
+# normalize capacities
+max_cap = max(caps)
+min_cap = min(caps)
+for client_id in capacities:
+    capacities[client_id] = (capacities[client_id] - min_cap) / (max_cap - min_cap)
+
 for dataset in dataset_info:
-    if not os.path.exists(dataset):
-        os.mkdir(dataset)
+    print(dataset)
+    if not os.path.exists(dataset+"_cap"):
+        os.mkdir(dataset+"_cap")
+    small_model_str = small_model[dataset]
+    large_model_str = large_model[dataset]
     dummy_input = torch.randn(dataset_info[dataset])
-    min_macs, min_params = profile(model_zoo['nasbench'], inputs=(dummy_input, ), verbose=False)
-    print(f"min MACs: {min_macs}, min params: {min_params}")
-    for model_name in ['resnet18', 'shufflenet']:
-        max_macs, max_params = profile(model_zoo['resnet18'], inputs=(dummy_input, ), verbose=False)
-        print(f"max MACs: {max_macs}, max MACs: {max_params}")
-        client_params = []
-        client_ids = []
-        client_macs = []
-        for client_id in computation:
-            client_params.append((max_params - min_params) * computation[client_id] + min_params)
-            client_macs.append((max_macs - min_macs) * computation[client_id] + min_macs)
-            client_ids.append(client_id)
-        records = list(zip(client_ids, client_macs, client_params))
-        with open(f'{dataset}/{model_name}-realistic.csv', 'w') as f:
-            writer = csv.writer(f)
-            writer.writerow(['id', 'macs', 'params'])
-            writer.writerows(records)
+    min_macs, min_params = profile(model_zoo[small_model_str], inputs=(dummy_input, ), verbose=False)
+    print(min_macs, min_params)
+    max_macs, max_params = profile(model_zoo[large_model_str], inputs=(dummy_input, ), verbose=False)
+    print(max_macs, max_params)
+    client_params = []
+    client_ids = []
+    client_macs = []
+    for client_id in computation:
+        client_params.append((max_params - min_params) * capacities[client_id] + min_params)
+        client_macs.append((max_macs - min_macs) * capacities[client_id] + min_macs)
+        client_ids.append(client_id)
+    records = list(zip(client_ids, client_macs, client_params))
+    with open(f'{dataset}_cap/realistic.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['id', 'macs', 'params'])
+        writer.writerows(records)
 
        
